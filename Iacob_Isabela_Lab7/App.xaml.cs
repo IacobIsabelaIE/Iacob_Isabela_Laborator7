cd8748fa3ecab2cfd21fe7c2073ac84627d5e66b
IacobIsabelaIE <isabela.iacob@stud.ubbcluj.ro>
2023-12-01 00:14:40 +0200

    Laborator 8 - branch pt lab 7

--- Iacob_Isabela_Lab7/App.xaml.cs
+++ Iacob_Isabela_Lab7/App.xaml.cs
@@ -1,11 +1,26 @@
-﻿namespace Iacob_Isabela_Lab7;
+﻿using Iacob_Isabela_Lab7.Data;
+
+namespace Iacob_Isabela_Lab7;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
-
-		MainPage = new AppShell();
-	}
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new AppShell();
+    }
 }
