e8c62765b4b7e9eecf9dc09290a34589a6d91ef1
IacobIsabelaIE <isabela.iacob@stud.ubbcluj.ro>
2023-12-10 23:34:05 +0200

    Laborator9 - iacob isabela

--- Iacob_Isabela_Lab7/Data/ShoppingListDatabase.cs
+++ Iacob_Isabela_Lab7/Data/ShoppingListDatabase.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 using Iacob_Isabela_Lab7.Models;
 using System.Linq;
 using System.Text;
-
+using SQLite;
+using SQLiteNetExtensions;
 namespace Iacob_Isabela_Lab7.Data
 {
     public class ShoppingListDatabase
@@ -15,7 +16,34 @@ namespace Iacob_Isabela_Lab7.Data
         {
             _database = new SQLiteAsyncConnection(dbPath);
             _database.CreateTableAsync<ShopList>().Wait();
+            _database.CreateTableAsync<Product>().Wait();
+            _database.CreateTableAsync<ListProduct>().Wait();
         }
+
+        public Task<int> SaveProductAsync(Product product)
+        {
+            if (product.ID != 0)
+            {
+                return _database.UpdateAsync(product);
+            }
+            else
+            {
+                return _database.InsertAsync(product);
+            }
+        }
+
+        public Task<int> DeleteProductAsync(Product product)
+        {
+            return _database.DeleteAsync(product);
+        }
+
+
+        public Task<List<Product>> GetProductsAsync()
+        {
+            return _database.Table<Product>().ToListAsync();
+        }
+
+
         public Task<List<ShopList>> GetShopListsAsync()
         {
             return _database.Table<ShopList>().ToListAsync();
---
+++ Iacob_Isabela_Lab7/ProductPage.xaml.cs
@@ -0,0 +1,28 @@
+using Iacob_Isabela_Lab7.Models;
+
+namespace Iacob_Isabela_Lab7;
+
+public partial class ProductPage : ContentPage
+{
+	public ProductPage()
+	{
+		InitializeComponent();
+	}
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var product = (Product)BindingContext;
+        await App.Database.SaveProductAsync(product);
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var product = (Product)BindingContext;
+        await App.Database.DeleteProductAsync(product);
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+}
